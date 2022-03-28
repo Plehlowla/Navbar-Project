dd581945148a6edbe1b5af7de88f04fef5f901c0
Pah Reh <45471747+Plehlowla@users.noreply.github.com>
2022-03-28 06:55:31 -0500

    finished project, finalized app.js

--- app.js
+++ app.js
@@ -5,13 +5,34 @@
 // toggle - toggles class
 
 
-const btn = document.querySelector('.nav-toggle');
+const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
 
-btn.addEventListener('click', function(){
-    if(links.classList.contains('show-links')){
-        links.classList.remove('show-links');
-    }else{
-        links.classList.add('show-links');
-    }
+navToggle.addEventListener('click', function(){
+    // console.log(links.classList);
+    // console.log(links.classList.contains('random'));
+    // console.log(links.classList.contains('links'));
+    // if(links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }else{
+    //     links.classList.add('show-links');
+    // }
+    
+    return links.classList.toggle('show-links');
 })
+
+
+
+
+
+
+
+
+// my own version
+// btn.addEventListener('click', function(){
+//     if(links.classList.contains('show-links')){
+//         links.classList.remove('show-links');
+//     }else{
+//         links.classList.add('show-links');
+//     }
+// })
